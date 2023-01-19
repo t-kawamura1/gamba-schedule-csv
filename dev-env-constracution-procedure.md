00eec5d6a42636aeedc6f1dd05c4c6a91e70031f
tkawamura <tkawamura.stayhome@gmail.com>
2023-01-19 22:03:18 +0900

    完成

---
+++ dev-env-constracution-procedure.md
@@ -0,0 +1,25 @@
+<!-- 下記が成功例 -->
+1. ワーキングディレクトリの指定（-w /home/app）コンテナを命名(--name pptr)、権限設定（--cap-add=SYS_ADMIN）、バインドマウント（-v /Users/tkawamura/javascript/soccer-schedule:/home/app）、イメージ名指定、bash立ち上げ
+```bash
+$ docker run -it -w /home/app --name pptr --cap-add=SYS_ADMIN -v /Users/tkawamura/javascript/soccer-schedule:/home/app  ghcr.io/puppeteer/puppeteer:19.5.2 bash
+```
+
+2. プロジェクト作成
+```bash
+$ npm init -y
+```
+
+3. puppeteerインストール
+```bash
+$ npm i puppeteer
+```
+
+4. csv-parseインストール
+```bash
+$ npm i csv-parse
+```
+
+5. iconv-liteインストール(Shift-JISのCSVを開く場合は必要)
+```bash
+$ npm i iconv-lite
+```
---
+++ gamba-schedule.js
@@ -0,0 +1,67 @@
+const pptr = require('puppeteer');
+const fs = require('fs');
+const stringifySync = require("csv-stringify/sync");
+
+(async () => {
+  const browser = await pptr.launch();
+  const page = await browser.newPage();
+
+  const currentYear = new Date().getFullYear();
+  await page.goto(`https://www.gamba-osaka.net/game/index/season/${currentYear}`);
+
+  /** 試合ごとのトップ要素 */
+  const gameSelector = '.game_schedule_list > li';
+
+  const game = await page.$$eval(gameSelector, g => g.map((li, i) => {
+    const currentYear = new Date().getFullYear();
+
+    /** 対戦相手 */
+    const vsTeam = li.querySelector('.game_schedule_list_detail_team div').textContent;
+
+    /** ゲームカテゴリと試合のナンバリング */
+    const section = li.querySelector('.game_schedule_list_sub p').textContent
+      .split(' ').reverse().join(' ');
+    const monthDayArr = li.querySelector('.game_schedule_list_detail_sche_date span').textContent.split('.');
+
+    /** 開催日 */
+    const startDate = `${currentYear}/${monthDayArr[0]}/${monthDayArr[1]}`;
+
+    /** 開始時間 */
+    const startTime = li.querySelector('.game_schedule_list_detail_sche_time').textContent;
+
+    const startTimeArr = startTime.split(':')
+    /** 終了時間 */
+    const endTime = startTime.length === 0
+      ? ''
+      : `${Number(startTimeArr[0]) + 2}:${startTimeArr[1]}`
+
+    /** 日程の補足 */
+    const description = li.querySelector('.game_schedule_list_detail_sche_or') == null
+      ? ''
+      : '日程未確定';
+
+    /** 開催場所 */
+    const location = li.querySelector('.game_schedule_list_detail_sche_sta').textContent;
+
+    return {
+      subject: `${vsTeam} ${section}`,
+      startDate,
+      startTime,
+      endDate: '',
+      endTime,
+      allDayEvent: startTime.length === 0 ? true : false,
+      description,
+      location,
+      private: false
+    }
+  }));
+  // console.log(game);
+
+  const csvString = stringifySync.stringify(game, {
+    header: true
+  });
+
+  fs.writeFileSync(`gamba_schedule_${currentYear}.csv`, csvString);
+
+  await browser.close()
+})();
